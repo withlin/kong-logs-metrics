ce397f23bf844e0ad9ce15577b0db88880bb0ba4
WithLin96 <248628578@qq.com>
2018-05-02 17:25:26 +0800

    refactor code

--- controller/login/login.go
+++ controller/login/login.go
@@ -18,10 +18,10 @@ func PostCheckLogin(c *gin.Context) {
 	var loginCommand User
 	if err := c.ShouldBindJSON(&loginCommand); err == nil {
 		fmt.Println("========================" + loginCommand.Password)
-		if loginCommand.Username == "" || loginCommand.Password == "" {
-			c.JSON(http.StatusOK, gin.H{"message": "false", "data": "无效账户名或密码"})
-		} else if loginCommand.Username == "admin" && loginCommand.Password == "admin" {
+
+		if loginCommand.Username == "admin" && loginCommand.Password == "admin" {
 			c.JSON(http.StatusOK, gin.H{"message": "ok", "data": "登录成功"})
+
 		} else {
 			c.JSON(http.StatusOK, gin.H{"message": "false", "data": "账户名无效或密码无效"})
 		}
