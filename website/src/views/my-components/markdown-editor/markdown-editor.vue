9e82763e2d7f07f9281bb40a547a9ec79326f44b
WithLin96 <248628578@qq.com>
2018-05-09 14:44:48 +0800

    fix bug

--- website/src/views/my-components/markdown-editor/markdown-editor.vue
+++ website/src/views/my-components/markdown-editor/markdown-editor.vue
@@ -167,7 +167,7 @@ export default {
                                        "id":element._id,
                                        "consumer":'',
                                        "usetime":`${element._source.latencies.request} ms`,
-                                       "starttime":element._source.started_at
+                                       "starttime":this.convertUTCTimeToLocalTime(element._source.started_at)
                                });
 
                                
@@ -210,6 +210,25 @@ export default {
             let data={"pagesize":200,"pagenumber":(index-1)*200}
             this.handleMethod(data);
             // console.log(this.current);
+        },
+        convertUTCTimeToLocalTime(UTCDateString){
+            if(!UTCDateString){
+          return '-';
+        }
+        function formatFunc(str) {    //格式化显示
+          return str > 9 ? str : '0' + str
+        }
+        let date2 = new Date(UTCDateString);     //这步是关键
+        let year = date2.getFullYear();
+        let mon = formatFunc(date2.getMonth() + 1);
+        let day = formatFunc(date2.getDate());
+        let hour = date2.getHours();
+        let noon = hour >= 12 ? 'PM' : 'AM';
+        hour = hour>=12?hour-12:hour;
+        hour = formatFunc(hour);
+        let min = formatFunc(date2.getMinutes());
+        let dateStr = year+'-'+mon+'-'+day+' '+noon +' '+hour+':'+min;
+        return dateStr;
         }
     }
 };
