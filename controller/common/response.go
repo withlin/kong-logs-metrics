574819f1138f9b72a817b9cef186fedc10e83583
WithLin96 <248628578@qq.com>
2018-05-18 16:10:46 +0800

    fix

---
+++ controller/common/response.go
@@ -0,0 +1,41 @@
+package common
+
+import (
+	"kong-logs-metrics/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SendErrJSON(msg string, args ...interface{}) {
+	if len(args) == 0 {
+		panic("?? *gin.Context")
+	}
+	var c *gin.Context
+	var errNo = model.ErrorCode.ERROR
+	if len(args) == 1 {
+		theCtx, ok := args[0].(*gin.Context)
+		if !ok {
+			panic("?? *gin.Context")
+		}
+		c = theCtx
+	} else if len(args) == 2 {
+		theErrNo, ok := args[0].(int)
+		if !ok {
+			panic("errNo ???")
+		}
+		errNo = theErrNo
+		theCtx, ok := args[1].(*gin.Context)
+		if !ok {
+			panic("?? *gin.Context")
+		}
+		c = theCtx
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"errNo": errNo,
+		"msg":   msg,
+		"data":  gin.H{},
+	})
+	//????
+	c.Abort()
+}
